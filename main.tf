bdba8dcf73c0f9b07b0caf0ed5f6dec1621dfbf3
steve-msa <whpark70@kakao.com>
2022-01-09 13:44:52 +0900

    policy에서 공백 제거

--- main.tf
+++ main.tf
@@ -72,19 +72,19 @@ resource "aws_iam_role" "ms-node" {
   name = "${local.cluster_name}.node"
 
   assume_role_policy = <<POLICY
-  {
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Principal": {
-                  "Service": "ec2.amazonaws.com"
-              },
-              "Action": "sts.AssumeRole"
-          }
-      ]
-  }
-  POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Principal": {
+              "Service": "ec2.amazonaws.com"
+          },
+          "Action": "sts.AssumeRole"
+      }
+  ]
+}
+POLICY
 }
 
 # node policy
