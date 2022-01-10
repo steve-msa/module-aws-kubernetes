2cc745aa17636f7571a7dcfa0ce72a1ea22f656c
steve-msa <whpark70@kakao.com>
2022-01-10 22:48:54 +0900

    인증서 테스트 #2

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,8 @@ output "eks_cluster_name" {
 }
 
 output "eks_cluster_certificate_data" {
-  value = aws_eks_cluster.ms-up-running.certificate_authority.0.data
+  value = aws_eks_cluster.ms-up-running.certificate_authority[0].data
 }
-
-#  x509: certificate signed by unknown authority 발생으로 추가
-# output "eks_cluster_auth_token" {
-#   value = data.aws_eks_cluster_auth.ms-up-running.token
-# }
 
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.ms-up-running.endpoint
